bbb33dc2dfdef9121b056951d3ac1bef42bb608f
Ziyang Zhou <ziyang.zhou@outlook.com>
2021-06-26 05:22:01 +0000

    ? fix booting

--- bpfloader/BpfLoader.cpp
+++ bpfloader/BpfLoader.cpp
@@ -194,8 +194,10 @@ int main(int argc, char** argv) {
             ALOGE("If this triggers randomly, you might be hitting some memory allocation "
                   "problems or startup script race.");
             ALOGE("--- DO NOT EXPECT SYSTEM TO BOOT SUCCESSFULLY ---");
+#if 0 // HACKED
             sleep(20);
             return 2;
+#endif
         }
     }
 
